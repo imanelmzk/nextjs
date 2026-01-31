0b2b9916263565e6583232e1b806865c8946ad3e
imane.lmzk <imane.lmzk@gmail.com>
2026-01-31 16:46:52 +0100

    A New Update in the Code

---
+++ src/app/articles/[title]/loading.js
@@ -0,0 +1,11 @@
+export default function Loading(){
+    return(
+        <div style={{
+            color: "orange",
+            fontSize: "30px",
+            padding: "50px"
+        }}>
+            ⏳ Chargement de l'article en cours...
+        </div>
+    )
+}
--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,4 +1,7 @@
-export default function PostsPage() {
+export default async function PostsPage() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    const todos = await response.json();
+
     return(
         <div>
             <h1>Posts Page</h1>
